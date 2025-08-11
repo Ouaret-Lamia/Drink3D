ca37f4b76f709fb626dcaeacfd624962503d95e2
Lamia <ouaretlamia04@gmail.com>
2025-08-11 17:24:51 +0200

    Implemented Hero scroll animation

---
+++ src/components/Coffees.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Coffees = () => {
+  return (
+    <section id='coffees' className='noisy' >
+        <img src="/images/bean.png" alt="l-bean" id='c-left-bean' />
+        <img src="/images/bean.png" alt="r-bean" id='c-right-bean' />
+    </section>
+  )
+}
+
+export default Coffees
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -37,14 +42,36 @@ const Hero = () => {
       })
       .to(".right-bean-up", { y: 200 }, 0)
       .to(".left-bean-up", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 65%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
     <>
-      <section id="hero" className="noisy">
+      <section
+        id="hero"
+         className="noisy"
+      >
         <h1 className="title">ESPRESSO</h1>
-        <img src="/images/bean.png" alt="bean" className="left-bean-up" />
-        <img src="/images/bean.png" alt="bean" className="right-bean-up" />
+        <img src="/images/bean.png" alt="bean" className="left-bean" />
+        <img src="/images/bean.png" alt="bean" className="right-bean" />
 
         <div className="body">
           <div className="content">
@@ -56,7 +83,7 @@ const Hero = () => {
               </p>
             </div>
 
-            <div className="view-cocktails">
+            <div className="view-coffees">
               <p className="subtitle">
                 Our coffee is crafted with care to awaken your senses and turn
                 every sip into a moment of quiet luxury.
@@ -66,6 +93,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
